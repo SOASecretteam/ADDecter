e04882e4f27d1e17ec27b12b8f470b236dc08661
ru81git454 <ru81o454@gmail.com>
2018-11-08 00:02:04 +0800

    database orm

--- app.py
+++ app.py
@@ -6,11 +6,39 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+#data base
+import os
+from flask_sqlalchemy import SQLAlchemy
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 app=Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+
+class PictureURL(db.Model):
+    __tablename__ = 'website'
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(64), unique=True)
+    WebEntity = db.relationship('WebEntity', backref='pictureURL') #relationship between PictureURL|WebEntity
+
+    def __repr__(self):
+        return '<PictureURL %r>' % self.url
+
+
+class WebEntity(db.Model):
+    __tablename__ = 'Entity'
+    id = db.Column(db.Integer, primary_key=True)
+    Entity = db.Column(db.String(64), unique=True, index=True)
+    website_id = db.Column(db.Integer, db.ForeignKey('website.id')) #relationship between PictureURL|WebEntity
+
+    def __repr__(self):
+        return '<WebEntity %r>' % self.Entity
 
 class UrlForm(FlaskForm):
     url = StringField('Please Enter the picture url', validators=[DataRequired()])
@@ -35,3 +63,19 @@ def index():
 
 if __name__ == '__main__':
     manager.run()
+
+test_url=PictureURL(url='https://www.youtube.com/watch?v=zRwy8gtgJ1A')
+test_url2=PictureURL(url='https://github.com/bradtraversy/myflaskapp')
+test_url3=PictureURL(url='https://www.bootstrapcdn.com/bootswatch/')
+web=WebEntity(Entity='sky',website_id='test_url')
+web2=WebEntity(Entity='building',website_id='test_url2')
+web3=WebEntity(Entity='lake',website_id='test_url3')
+
+db.session.add(test_url)
+db.session.add(test_url2)
+db.session.add(test_url3)
+db.session.add(web)
+db.session.add(web2)
+db.session.add(web3)
+db.session.commit()
+print(test_url.id)
