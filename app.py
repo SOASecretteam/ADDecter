fe4e17d5bae473a14113fd23dc2389f43700af10
ru81git454 <ru81o454@gmail.com>
2018-11-07 17:48:44 +0800

    http error

--- app.py
+++ app.py
@@ -3,6 +3,15 @@ from flask_script import Manager
 
 app=Flask(__name__)
 manager = Manager(app)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 @app.route('/')
 def index():
     return render_template('index.html')
