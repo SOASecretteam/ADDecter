1803999ccf15f679eb39e2024ef3a8b89286484f
lynnpan168 <lynnpan168@iss.nthu.edu.tw>
2018-11-13 12:24:18 +0800

    first version

--- lib/googleapi.py
+++ lib/googleapi.py
@@ -5,89 +5,78 @@ from google.cloud.vision import types
 from google.protobuf.json_format import MessageToJson
 import json
 
-#authorize
-credentials = service_account.Credentials.from_service_account_file('C:/Users/Lynn/Desktop/SOA/key.json')
-client = vision.ImageAnnotatorClient(credentials=credentials)
-
 class Api:
-    def __init__(self, key_file = '/config/key.json'):
+    def __init__(self, key_file = 'ADDecter/config/key.json'):
         credentials = service_account.Credentials.from_service_account_file(key_file)
-        client = vision.ImageAnnotatorClient(credentials=credentials)
+        self.client = vision.ImageAnnotatorClient(credentials=credentials)
+    
+    def image_processing(self, image_path):
+        if image_path.startswith('http') or image_path.startswith('gs:'):
+            image = types.Image()
+            image.source.image_uri = image_path
+        else: 
+            with io.open(image_path, 'rb') as image_file:
+                content = image_file.read()
+            image = types.Image(content=content) 
+        return image
 
-def image_processing(path):
-    if path.startswith('http') or path.startswith('gs:'):
-        image = types.Image()
-        image.source.image_uri = path
-    else: 
-        with io.open(path, 'rb') as image_file:
-            content = image_file.read()
-        image = types.Image(content=content) 
-    return image
+    def face(self, image_path):
+        image = self.image_processing(image_path)
+        face = self.client.face_detection(image=image)
+        return face
 
-def face(image_path):
-    image = image_processing(image_path)
-    face = client.face_detection(image=image)
-    return face
+    def lables(self, image_path):
+        image = self.image_processing(image_path)
+        lable = self.client.label_detection(image=image)
+        return lable
 
-def lables(image_path):
-    image = image_processing(image_path)
-    lable = client.label_detection(image=image)
-    return lable
+    def logo(self, image_path):
+        image = self.image_processing(image_path)
+        logo = self.client.logo_detection(image=image)
+        return logo
 
-def logo(image_path):
-    image = image_processing(image_path)
-    logo = client.logo_detection(image=image)
-    return logo
+    def landmark(self, image_path):
+        image = self.image_processing(image_path)
+        landmark = self.client.landmark_detection(image=image)
+        return landmark
 
-def landmark(image_path):
-    image = image_processing(image_path)
-    landmark = client.landmark_detection(image=image)
-    return landmark
+    def text(self, image_path):
+        image = self.image_processing(image_path)
+        text = self.client.text_detection(image=image)
+        return text
 
-def text(image_path):
-    image = image_processing(image_path)
-    text = client.text_detection(image=image)
-    return text
+    def properties(self, image_path):
+        image = self.image_processing(image_path)
+        properties = self.client.image_properties(image=image)
+        return properties
 
-def properties(image_path):
-    image = image_processing(image_path)
-    properties = client.image_properties(image=image)
-    return properties
+    def safe_properties(self, image_path):
+        image = self.image_processing(image_path)
+        safe_properties = self.client.safe_search_detection(image=image)
+        return safe_properties
 
-def safe_properties(image_path):
-    image = image_processing(image_path)
-    safe_properties = client.safe_search_detection(image=image)
-    return safe_properties
+    def web_entities(self, image_path):
+        image = self.image_processing(image_path)
+        web_entities = self.client.web_detection(image=image)
+        return web_entities
 
-def web_entities(image_path):
-    image = image_processing(image_path)
-    web_entities = client.web_detection(image=image)
-    return web_entities
+    def document_text(self, image_path):
+        image = self.image_processing(image_path)
+        document_text = self.client.document_text_detection(image=image)
+        return document_text
 
-def document_text(image_path):
-    image = image_processing(image_path)
-    document_text = client.document_text_detection(image=image)
-    return document_text
+    def objects(self, image_path):
+        image = self.image_processing(image_path)
+        objects = self.client.object_localization(image=image)
+        return objects
 
-def objects(image_path):
-    image = image_processing(image_path)
-    objects = client.object_localization(image=image)
-    return objects
-
-def crop_hints_params(image_path):
-    image = image_processing(image_path)
-    crop_hints_params = vision.types.CropHintsParams(aspect_ratios=[1.77])
-    image_context = vision.types.ImageContext(
-        crop_hints_params=crop_hints_params)
-    crop = client.crop_hints(image=image, image_context=image_context)
-    hints = crop.crop_hints_annotation.crop_hints
-    # Get bounds for the first crop hint using an aspect ratio of 1.77.
-    vertices = hints[0].bounding_poly.vertices
-    return vertices
-
-# compute the outputs
-crop = objects(uri)
-
-# write to json file
-with open("file.json", 'w') as fjs:
-    fjs.write(MessageToJson(crop))
+    def crop_hints_params(self, image_path):
+        image = self.image_processing(image_path)
+        crop_hints_params = vision.types.CropHintsParams(aspect_ratios=[1.77])
+        image_context = vision.types.ImageContext(
+            crop_hints_params=crop_hints_params)
+        crop = self.client.crop_hints(image=image, image_context=image_context)
+        hints = crop.crop_hints_annotation.crop_hints
+        # Get bounds for the first crop hint using an aspect ratio of 1.77.
+        vertices = hints[0].bounding_poly.vertices
+        return vertices
--- lib/pages_with_matching_images.py
+++ lib/pages_with_matching_images.py
@@ -1,5 +1,6 @@
 import lib.googleapi as googleapi
 import lib.testsample as testsample
+
 web_detection=googleapi.annotate(testsample.pictureurl)
 class pages_with_matching_images():
     def __init__(self,web_detection):
